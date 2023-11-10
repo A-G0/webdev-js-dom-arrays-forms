98d2eb1a0c2650dd722ffb1a7019300942870872
A-G0 <c3011305@hallam.shu.ac.uk>
2023-11-10 10:56:58 +0000

    added more java

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 (function() {
   // code starts here
 document.getElementById("myHeading").innerHTML = "Abdusalam";
+document.querySelector("nav ul li").setAttribute("class", "currentPage");
+// example
+document
+.querySelector("nav ul li a")
+.setAttribute("href", "https://www.google.co.uk/");
+ 
 
-  // code ends here
+// code ends here
 })();
