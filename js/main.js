417e88d9d88a134333c678dd09c51d9dd9f5c3ff
A-G0 <c3011305@hallam.shu.ac.uk>
2023-11-17 09:47:17 +0000

    add colours

--- js/main.js
+++ js/main.js
@@ -8,7 +8,15 @@ document
 .querySelector("nav ul li a")
 .setAttribute("href", "https://www.google.co.uk/");
  // adding events
- document.querySelector(".red").addEventListener 
-
+ document.querySelector(".red").addEventListener('click', function(ev){
+  document.querySelector('body').setAttribute('class', "redBack");
+  })
+  document.querySelector(".blue").addEventListener('click', function(ev){
+    document.querySelector('body').setAttribute('class', "blueBack");
+    })
+    document.querySelector(".green").addEventListener('click', function(ev){
+      document.querySelector('body').setAttribute('class', "greenBack");
+      })
+     
 // code ends here
 })();
